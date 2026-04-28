56671d1d21ca42f5a508f77f6b546997ebe7b8c8
Melnikov Denis <melnikov.denis.aleksandrovich@gmail.com>
2026-04-29 01:16:20 +0300

    Fixed building master

--- mods/tty-graphics/main.cpp
+++ mods/tty-graphics/main.cpp
@@ -56,7 +56,7 @@ class TTYgraph : public BmServerModule {
         h = wsz.ws_row;
         std::cout << "\x1b[2J"; // clear screen
     }
-    
+
     void frameEnd() {
     }
 
@@ -105,7 +105,7 @@ class TTYgraph : public BmServerModule {
                 auto tile = m_map->at({j, i});
                 if (!tile->units().empty())
                     clr = ESC_RED, chr = "@";
-                else if (!tile->isWalkable())
+                else if (tile->type() == modlib::Tile::BasicType::Wall)
                     clr = ESC_RST, chr = "#";
                 else
                     clr = ESC_GRY, chr = ".";
