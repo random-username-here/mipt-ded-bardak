e9ace513d54b091fda336ef2b5887e7e6910efd7
DoDo6b <katynskii.kiu@phystech.edu>
2026-04-30 20:04:23 +0300

    added enum class Timer::Type that defines the timer's operating mode

--- modlib-ifc/inc/Timer.hpp
+++ modlib-ifc/inc/Timer.hpp
@@ -8,19 +8,30 @@ namespace modlib {
 class Timer : public Mod
 {
 public:
-    using Callback = std::function<void(void)>;
     enum class Stage
     {
         ON_UPDATE = 0,
         ON_UPDATE_DONE
     };
-    using Tick = size_t;
-    using TimerID = std::pair<Tick, uint64_t>;
+    enum class Type
+    {
+        COUNTDOWN = 0,
+        CYCLE
+    };
+    using Tick     = size_t;
+    using Callback = std::function<void(void)>;
+    struct TimerID
+    {
+        Type m_type;
+        Tick m_tick;
+        uint64_t m_ID;
+    };
 
     virtual TimerID setTimer (
-        Tick delay,
+        Tick     delay,
         Callback callback,
-        Stage type
+        Stage    stage = Stage::ON_UPDATE,
+        Type     type   = Type::COUNTDOWN
     ) = 0;
 
     virtual void cancelTimer (
