66db46ac3bc57a0653bf67ecc47a0a0a1dc11042
DoDo6b <katynskii.kiu@phystech.edu>
2026-04-30 20:04:23 +0300

    added enum class Timer::Type that defines the timer's operating mode

--- mods/timer/src/timer_impl.cpp
+++ mods/timer/src/timer_impl.cpp
@@ -1,103 +1,106 @@
 #include <stdexcept>
+#include <functional>
 #include <random>
+#include <map>
+#include <set>
+#include <cstring>
 #include <queue>
-#include "../timer_impl.hpp"
+#include "Timer.hpp"
 
 
-std::string_view 
-Timer::id () const
+class Timer : public modlib::Timer
 {
-    return "DoDoeb.timer_mngr";
-}
-
-std::string_view 
-Timer::brief () const
-{
-    return "Singleton timer module by DoDoeb";
-}
-
-ModVersion
-Timer::version () const
-{
-    return ModVersion (1, 1, 6);
-}
-
-
-Timer::TimerID 
-Timer::setTimer (
-    Tick delay,
-    Callback callback,
-    Stage type
-)
-{
-    if (callback == nullptr)
+public:
+    TimerID setTimer (
+        Tick     delay,
+        Callback callback,
+        Stage    stage,
+        Type     type
+    ) override
     {
-        throw std::runtime_error ("invalid callback provided");
+        if (callback == nullptr)
+        {
+            throw std::runtime_error ("invalid callback provided");
+        }
+
+        Tick tickStamp = m_tickCounter + delay;
+
+        TimerID id = {
+            .m_type = type,
+            .m_tick = delay,
+            .m_ID = rand ()
+        };
+
+        m_tickStamps[tickStamp][id.m_ID] = {
+            .m_stage    = stage,
+            .m_cycle    = type == Type::CYCLE ? delay : 0,
+            .m_callback = callback
+        };
+        
+        
+        return id;
     }
 
-    Tick tickStamp = m_tickCounter + delay;
-
-    TimerID id = {
-        tickStamp,
-        rand ()
-    };
-
-    m_tickStamps[tickStamp][id.second] = {
-        type,
-        callback
-    };
-    
-    
-    return id;
-}
-
-void
-Timer::cancelTimer (
-    Timer::TimerID& id
-)
-{
-    m_tickStamps[id.first].erase (id.second);
-
-    id = {0, 0};
-}
-
-
-void
-Timer::tick ()
-{
-    m_tickCounter++;
-
-    if (m_tickStamps[m_tickCounter].empty () == false)
+    void cancelTimer (
+        TimerID& id
+    ) override
     {
-        std::queue<Callback> callbackbackQueue;
+        m_tickStamps[id.m_tick].erase (id.m_ID);
 
-        for (const auto [_, entry] : m_tickStamps[m_tickCounter])
-        {
-            if (entry.first == Stage::ON_UPDATE)
-            {
-                entry.second ();
-            }
-            else
-            {
-                callbackbackQueue.push (entry.second);
-            }
-        }
-        m_tickStamps.erase (m_tickCounter);
+        std::memset (&id, 0, sizeof (TimerID));
+    }
 
-        while (callbackbackQueue.empty () == false)
+    void tick () override
+    {
+        m_tickCounter++;
+
+        if (m_tickStamps[m_tickCounter].empty () == false)
         {
-            callbackbackQueue.front () ();
-            callbackbackQueue.pop ();
+            std::queue<Callback> callbackbackQueue;
+
+            for (const auto [id, entry] : m_tickStamps[m_tickCounter])
+            {
+                if (entry.m_stage == Stage::ON_UPDATE)
+                {
+                    entry.m_callback ();
+                }
+                else
+                {
+                    callbackbackQueue.push (entry.m_callback);
+                }
+                if (entry.m_cycle)
+                {
+                    m_tickStamps[m_tickCounter + entry.m_cycle][id] = entry;
+                }
+            }
+            m_tickStamps.erase (m_tickCounter);
+
+            while (callbackbackQueue.empty () == false)
+            {
+                callbackbackQueue.front () ();
+                callbackbackQueue.pop ();
+            }
         }
     }
-}
 
-Timer::Tick
-Timer::getTicksSinceCreation ()
-{
-    return m_tickCounter;
-}
+    Tick             getTicksSinceCreation ()       override { return m_tickCounter; }
 
+    std::string_view id                    () const override { return "DoDoeb.timer_mngr"; }
+    std::string_view brief                 () const override { return "Singleton timer module by DoDoeb"; }
+    ModVersion       version               () const override { return ModVersion (1, 2, 0); }
+
+private:
+    struct CallbackEntry
+    {
+        Stage    m_stage;
+        Tick     m_cycle;
+        Callback m_callback;
+    };
+
+    
+    Tick m_tickCounter = 0;
+    std::map<Tick, std::map<uint64_t, CallbackEntry>> m_tickStamps;
+};
 
 
 extern "C" Mod *modlib_create (ModManager *mm)
