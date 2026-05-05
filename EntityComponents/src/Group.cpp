0e8fa7a46ab1a4cece770fa9d7752dbe4ab882c1
DoDo6b <katynskii.kiu@phystech.edu>
2026-05-05 16:07:50 +0300

    implemented Group component

---
+++ EntityComponents/src/Group.cpp
@@ -0,0 +1,23 @@
+#include "../inc/ECbasis.hpp"
+using namespace EC;
+
+
+void Social::Group::setGroupID (Social::Group::GID gid)
+{
+    m_groupID = gid;
+
+    EvGroupChanged.emit ();
+}
+
+void Social::Group::setGroupID (bmsg::Char64 group)
+{
+    m_groupID = group.as_u64;
+
+    EvGroupChanged.emit ();
+}
+
+
+Social::Group::GID Social::Group::getGroupID () const
+{
+    return m_groupID;
+}
