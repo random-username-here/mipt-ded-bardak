6297794dd89e1b67bf581dec22b4be54ee44fe4c
ivan_a <mai.abrutinid@gmail.com>
2026-05-11 21:41:33 +0300

    feat: add ghost client, start fixing bugs and redundand dependencies

---
+++ clients/ghost/utils/proto/binmsg.hpp
@@ -0,0 +1,130 @@
+/**
+ * \file
+ * \brief top-level `bmsg` message format
+ * \author Ivan Didyk
+ * \date 2026-04-20
+ *
+ * This header is mainly for standardizing plugin API-s.
+ */
+#pragma once
+#include <cassert>
+#include <cstdint>
+#include <string_view>
+#include <cstring>
+
+namespace bmsg {
+
+/** 
+ * \brief Short string of 8 characters
+ */
+union Char64 {
+    uint64_t as_u64;
+    char as_chars[8];
+
+    constexpr Char64() : as_u64(0) {}
+    constexpr Char64(uint64_t v) : as_u64(v) {}
+
+    constexpr Char64(std::string_view s) : as_chars{0,0,0,0,0,0,0,0} { 
+        if (s.size() > 8) { 
+            throw "s.size() should be <= 8"; 
+        }
+        
+        for (size_t i = 0; i < s.size(); ++i) {
+            as_chars[i] = s[i]; 
+        }
+    }
+
+    constexpr Char64(const char* c) : Char64(std::string_view(c)) {}
+
+    constexpr size_t size() const {
+        size_t s = 0;
+        while (s < 8 && as_chars[s] != '\0') ++s;
+        return s;
+    }
+
+    constexpr Char64 &operator=(const std::string_view &s) {
+        *this = Char64(s);
+        return *this;
+    }
+
+    constexpr operator uint64_t() const { return as_u64; }
+    constexpr operator std::string_view() const { return std::string_view(as_chars, size()); }
+};
+
+struct Char64Hasher {
+    size_t operator()(const bmsg::Char64& c) const {
+        return std::hash<uint64_t>{}(c.as_u64);
+    }
+};
+
+constexpr inline bool operator==(Char64 lhs, Char64 rhs) { return lhs.as_u64 == rhs.as_u64; }
+constexpr inline bool operator!=(Char64 lhs, Char64 rhs) { return !(lhs == rhs); }
+
+/** 
+ * \brief Message ID type
+ */
+using Id = uint32_t;
+
+/** 
+ * \brief BMSG header structure
+ * Flags are currently not being used by standard for anything.
+ * Note on this not being packed: this is due to Char64 not being POD because
+ * of convenience constructors. But this works fine, because this is packed by hand.
+ */
+struct Header {
+    Char64 pref;
+    Char64 type;
+    Id id;
+    uint16_t len;
+    uint16_t flags;
+};
+
+/** View to some BMSG message with non-decoded header */
+class RawMessage {
+    std::string_view m_data;
+public:
+    RawMessage(std::string_view buf) :m_data(buf) {}
+
+    std::string_view data() { return m_data; }
+
+    /**
+     * \brief Obtain message header.
+     * If message is cut such that it is shorter than header,
+     * this will return nullptr. 
+     */
+    const Header *header() {
+        if (data().size() < sizeof(Header))
+            return nullptr;
+        return reinterpret_cast<const Header*>(data().data());
+    }
+
+    /** 
+     * \brief Message body as string view. 
+     * Will return nullptr stringview if message is not valid.
+     */
+    std::string_view body() {
+        if (!isCorrect())
+            return std::string_view();
+        return data().substr(sizeof(Header));
+    }
+
+    /** 
+     * \brief Pointer to message body
+     * Use this carefully, only if you checked body size beforehand.
+     */
+    const void *bodyPtr() {
+        return body().data();
+    }
+
+    /**
+     * \brief Is message's length correct?
+     * This does not care about contents of the body,
+     * and for things like missing arguments.
+     */
+    bool isCorrect() {
+        auto head = header();
+        if (!head) return false;
+        return data().size() == sizeof(Header) + head->len;
+    }
+};
+};
