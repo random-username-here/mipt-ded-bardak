77c122823669fbdf46754ace5eb2d5e08214d154
DoDo6b <katynskii.kiu@phystech.edu>
2026-05-14 15:33:39 +0300

    code styled

--- clients/adapter/main.cpp
+++ clients/adapter/main.cpp
@@ -6,76 +6,106 @@
 #include <sys/types.h>
 #include <iostream>
 #include <unistd.h>
-#include "client-core/base/client_base.hpp"
 
-class AdapterClient : public ClientBase {
-public:
-    AdapterClient(const std::string &ini, const char *prefix) : ClientBase(ini) {
-        registerOnPrefix(prefix, [this](const PanFrame &frame) {
-            return 0;
-        });
-    }
-};
-
-int main(int argc, char **argv) {
-    if (argc < 3) {
+int main (
+    int    argc,
+    char** argv
+)
+{
+    if (argc < 3)
+    {
         std::cerr << "usage: \"" << argv[0] << " *path2script* *settings.ini*" << std::endl;
+        
         return EXIT_FAILURE;
     }
 
     int pipe2ch[2];
     int pipe4ch[2];
 
-    if (pipe(pipe2ch) || pipe(pipe4ch)) {
-        perror("could not open pipe");
-        abort();
+    if (
+        pipe (pipe2ch) || 
+        pipe (pipe4ch)
+    )
+    {
+        perror ("could not open pipe");
+        abort ();
     }
 
-    pid_t pid = fork();
-    if (pid == -1) {
-        perror("fork failed");
+    pid_t pid = fork ();
+    if (  pid == -1)
+    {
+        perror ("fork failed");
 
-        close(pipe2ch[0]);
-        close(pipe2ch[1]);
-        close(pipe4ch[0]);
-        close(pipe4ch[1]);
+        close (pipe2ch[0]);
+        close (pipe2ch[1]);
+        close (pipe4ch[0]);
+        close (pipe4ch[1]);
 
-        abort();
+        abort ();
     }
 
-    if (pid == 0) { // child
-        if (dup2(pipe2ch[0], STDIN_FILENO) == -1) {
-            perror("dup2");
-            abort();
+    if (pid == 0)   // child
+    {
+        if (
+            dup2 (
+                pipe2ch[0],
+                STDIN_FILENO
+            ) == -1
+        )
+        {
+            perror ("dup2");
+            
+            close (pipe2ch[0]);
+            close (pipe2ch[1]);
+            close (pipe4ch[0]);
+            close (pipe4ch[1]);
+
+            abort ();
         }
-        close(pipe2ch[1]);
+        close (pipe2ch[1]);
 
-        if (dup2(pipe4ch[1], STDOUT_FILENO) == -1) {
-            perror("dup2");
-            abort();
+        if (
+            dup2 (
+                pipe4ch[1],
+                STDOUT_FILENO
+            ) == -1
+        )
+        {
+            perror ("dup2");
+
+            close (pipe2ch[0]);
+            close (pipe4ch[0]);
+            close (pipe4ch[1]);
+
+            abort ();
         }
-        close(pipe4ch[0]);
+        close (pipe4ch[0]);
 
-        execlp(argv[1], argv[1], NULL);
-        perror("execlp failed");
+        execlp (
+            argv[1],
+            argv[1],
+            NULL
+        );
+        perror ("execlp failed");
 
-        close(pipe2ch[0]);
-        close(pipe4ch[1]);
+        close (pipe2ch[0]);
+        close (pipe4ch[1]);
 
-        abort();
+        abort ();
     }
 
     // parent
-    close(pipe2ch[0]);
-    close(pipe4ch[1]);
+    close (pipe2ch[0]);
+    close (pipe4ch[1]);
 
-    int to_child   = pipe2ch[1];
-    int from_child = pipe4ch[0];
+    int fd2ch = pipe2ch[1];
+    int fd4ch = pipe4ch[0];
 
-    __gnu_cxx::stdio_filebuf<char> inbuf (from_child, std::ios::in );
-    __gnu_cxx::stdio_filebuf<char> outbuf(to_child,   std::ios::out);
-    std::istream s2ch(&inbuf );
-    std::ostream s4ch(&outbuf);
+    __gnu_cxx::stdio_filebuf<char> inbuf  (fd4ch, std::ios::in );
+    __gnu_cxx::stdio_filebuf<char> outbuf (fd2ch, std::ios::out);
+    
+    std::istream s2ch (&inbuf );
+    std::ostream s4ch (&outbuf);
 
     std::string prefix;
     s2ch >> prefix;
