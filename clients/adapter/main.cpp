c98097e572a209e9d31122123591549dcd70fe3b
DoDo6b <katynskii.kiu@phystech.edu>
2026-05-18 00:33:06 +0300

    multithreaded adapter

--- clients/adapter/main.cpp
+++ clients/adapter/main.cpp
@@ -1,20 +1,105 @@
-#include <iostream>
 #include "client-core/tcp/tcp_connection.hpp"
-#include "proxy/inc.hpp"
+#include "proxy/proxy.hpp"
 
-int main (
-    int    argc,
-    char** argv
-)
+#include <array>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <unistd.h>
+
+
+
+int main(int argc, char* argv[])
 {
-    if (argc < 3)
+    if (argc < 4)
     {
-        std::cerr << "usage: \"" << argv[0] << " *path2script* *settings.ini*" << std::endl;
-        
+        std::cerr << "Usage: " << argv[0] << " <program> <host> <port>\n";
         return EXIT_FAILURE;
     }
 
-    Proxy script (argv[1]);
+    try 
+    {
+        TcpConnection conn (
+            "localhost",
+            "3000"
+        );
+        Proxy         proxy (
+            argv[1]
+        );
+        
+        std::atomic<bool> running {true};
+        
+        std::thread S2Ch (
+            [&]()
+            {
+                while (running)
+                {
+                    PanFrame frame;
+                    auto status = conn.readFrame(frame);
+                    
+                    if (status == TcpConnection::IoStatus::OK)
+                    {
+                        proxy.write (
+                            frame.payload.data(), 
+                            frame.payload.size()
+                        );
+                        proxy.flush ();
+                    }
+                    else if (status != TcpConnection::IoStatus::TIMEOUT)
+                    {
+                        running = false;
+                        break;
+                    }
+                }
+            }
+        );
+        
+        std::thread Ch2S (
+            [&]()
+            {
+                std::array<uint8_t, BUFSIZ> buffer;
+                
+                while (running)
+                {
+                    ssize_t read = proxy.read(
+                        buffer.data (),
+                        buffer.size ()
+                    );
+                    
+                    if (read > 0)
+                    {
+                        PanFrame frame;
+                        frame.payload.assign (
+                            buffer.begin(),
+                            buffer.begin() + read
+                        );
+                        
+                        if (conn.writeRaw(frame.rawMessage()) != TcpConnection::IoStatus::OK)
+                        {
+                            running = false;
+                            break;
+                        }
+                    }
+                    else if (read <= 0)
+                    {
+                        running = false;
+                        break;
+                    }
+                }
+            }
+        );
+        
+        S2Ch.join ();
+        Ch2S.join ();
+        
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Fatal error: " << e.what () << std::endl;
+        return EXIT_FAILURE;
+    }
 
-    
+    return EXIT_SUCCESS;
 }
